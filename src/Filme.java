97e1399256f85bb4e869628534cfa02450628766
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-04-29 17:48:08 -0300

    commit XD

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Filme {
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaAvaliacoes;
+    private int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibirFilme() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos do filme: " + duracaoEmMinutos);
+    }
+
+    void avalia(double nota) {
+        somaAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double retornarMedia() {
+        return somaAvaliacoes / totalDeAvaliacoes;
+    }
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Filme filme = new Filme();
 
+        filme.nome = "Top Gun Maverick";
+        filme.anoDeLancamento = 2022;
+        filme.duracaoEmMinutos = 180;
+
+        filme.exibirFilme();
+        filme.avalia(8);
+        filme.avalia(5);
+        filme.avalia(10);
+
+        System.out.println("Total de avaliações: " + filme.getTotalDeAvaliacoes());
+
+        System.out.println(filme.retornarMedia());
     }
 }
