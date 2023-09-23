842fda6681da67aa1fe90879f815c9f40397d43c
Arthur Nunes Pereira <arthurnunesp@live.com>
2023-09-23 16:57:26 -0300

    commit utilizando HttpRequest e HttpResponse para pegar e exibir dados da API

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan =  new Scanner(System.in);
+        System.out.println("Digite o filme que você quer buscar:");
+        var filme = scan.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + filme + "&apikey=cebf13f2";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
